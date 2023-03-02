7b098a5e7d3bc77bbfb7a14dc7750c6ac903ccae
Dilshara Herath <delatck@gmail.com>
2023-03-03 00:53:33 +0530

    Third commit

---
+++ React/burger_joint/src/Components/PartOne.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PartOne() {
+  return (
+    <div>
+        <h1>I am Dela</h1>
+    </div>
+  )
+}
---
+++ React/burger_joint/src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import TopicBox from '../Components/TopicBox'
+
+export default function Home() {
+  return (
+    <div>
+        <h1>This is the home page</h1>
+        <TopicBox></TopicBox>
+    </div>
+  )
+}
